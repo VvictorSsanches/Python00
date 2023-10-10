0ee3ea0dca3f552b017a35d7fc9d46bc45622026
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-10-10 13:52:51 -0300

    att

--- Exercícios/Exercícios 31 ao 45/Exercícios35.py
+++ Exercícios/Exercícios 31 ao 45/Exercícios35.py
@@ -16,3 +16,15 @@ if a1 + a2 > a3 and a2 + a3 > a1 and a3 + a1 > a2:
     print('Prodemos formar um riangulo.')
 else:
     print ('Não podemos Formar um triangulo.')
+    
+
+if l2 + l3 > l1 and l2 + l1 > l3 and l1 + l2 > l3:
+    print('O triângulo \033[1;34mexiste\033[m', end='')
+    if l1 == l2 and l1 == l3:
+        print(' e é equilátero.')
+    elif l1 != l2 and l1 != l3:
+        print(' e é escaleno.')
+    l2 == l3 and l2 != l1:
+        print(' e é isósceles.') # Essa parte do isósceles dá pra por apenas um "else:"
+else:
+    print('O triângulo \033[1;31mnão\033[m existe.')
--- Exercícios/Exercícios 31 ao 45/Exercícios42.py
+++ Exercícios/Exercícios 31 ao 45/Exercícios42.py
@@ -2,3 +2,21 @@
 que tipo de triângulo será formado: - EQUILÁTERO: todos os lados iguais - ISÓSCELES: dois lados
 iguais, um diferente - ESCALENO: todos os lados diferentes
 '''
+print('-=-'*20)
+print('Analisador de Triangulos')
+print('-=-'*20)
+
+a1 = float(input('Insira o primeiro comprimento: '))
+a2 = float(input('Insira o segundo comprimento: '))
+a3 = float(input('insira o terceiro comprimento: '))
+
+if a2 + a3 > a1 and a2 + a1 > a3 and a1 + a2 > a3:
+    print('O tiangulo mexiste.')
+    if a1 == a2 and a1 != a3:
+        print('ela é Equilátero.')
+    elif a1 != a2 and a1 != a3:
+        print('ela é Escaleno.')
+    elif a1 == a2 and a1 != a3 and a1 == a3 and a1 != a2 and a2 == a3 and a2 != a1:
+        print('ela é Isósceles.')
+else:
+    print('O Triangulo não existe.')
