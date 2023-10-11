8fbff56c789bf70e823f80e64204f20f4ba0fa2c
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-10-11 10:39:11 -0300

    att

--- Exercícios/Exercícios 31 ao 45/Exercícios43.py
+++ Exercícios/Exercícios 31 ao 45/Exercícios43.py
@@ -4,3 +4,19 @@ Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo: - IMC a
 de 18,5: Abaixo do Peso - Entre 18,5 e 25: Peso Ideal - 25 até 30: Sobrepeso - 30 até
 40: Obesidade - Acima de 40: Obesidade Mórbida
 '''
+
+peso = float(input('Informe o seu peso: '))
+altura = float(input('Informe a sua altura: '))
+imc = peso / (altura**2)
+if  imc == 18.5: 
+    print('Seu IMC é {:.2f} você está abiaxo do peso'.format(imc))
+elif imc < 24.9:
+    print('Seu IMC é {:.2f} você está no peso normal'.format(imc))
+elif imc < 29.9:
+    print('Seu IMC é {:.2f} você está sobrepeso'.format(imc))
+elif imc < 34.9:
+    print('Seu IMC é {:.2f} você está com obesidade Grau 1'.format(imc))
+elif imc < 39.9:
+    print('Seu IMC é {:.2f} você está com o besidade Grau 2 (Severa)'.format(imc))
+else:
+    print('Seu IMC é {:.2f} você está com obesidade Grau 3 (mórbida)'.format(imc)) 
--- Exercícios/Exercícios 31 ao 45/Exercícios44.py
+++ Exercícios/Exercícios 31 ao 45/Exercícios44.py
@@ -2,3 +2,13 @@
 e condição de pagamento: - à vista dinheiro/cheque: 10% de desconto - à vista no cartão: 5% de
 desconto - em até 2x no cartão: preço formal - 3x ou mais no cartão: 20% de juros
 '''
+'''
+QUAL METODO DE PAGAMENTO 
+DINHIRO/CHQUE = 10% DESCONTO 
+CARTÃO A VISTA 5% DESCONTO 
+EM ATÉ 2X PREÇO NORMLA 
+MAIS Q 3X 20% JUROS
+'''
+valor = float(input('Valor do produto: '))
+pag = [1, 2, 3, 4]
+print('[1] Dinheiro, [2]')
