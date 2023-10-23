358fc3d8128a8994def8ac78ef16dbe286cf4771
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-10-23 16:04:29 -0300

    att1

---
+++ Aulas/Aula13.py
@@ -0,0 +1,22 @@
+#Estrutura de Repetição
+#laço C no intervalo(1,10) C = ( é a variavel de controle), Contador
+##passo
+#pega
+for c in range(1,10):
+    passo
+pega
+
+for c in range(0,3):
+    passo
+    pula
+passo
+pega
+
+for c in range(0,3):
+    if moeda:
+        pega
+    passo
+    pula
+passo
+pega 
+    
