d30ca96e1c08e25821b96205f3f77cc07c32f8eb
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-10-24 16:55:20 -0300

    att

--- Aulas/Aula13a.py
+++ Aulas/Aula13a.py
@@ -2,8 +2,20 @@
 for c in range(0, 6):
     print('oi')
 print('FIM')
-
+print('-=-'*5)
 
 for c in range(0, 6):
     print(c)
 print('FIM')
+print('-=-'*5)
+
+for c in range (0,4):
+    print('passo')
+    print('Pulas')
+print('passo')
+print('pegar')
+print('-=-'*5)
+
+for c in range(0, 10, 3):
+    print(c)
+print('fim')
