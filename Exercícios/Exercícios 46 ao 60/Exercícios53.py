3acdc64d4dbfff2ce12dfd9b9c1fb0cd26781e8e
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-10-26 16:33:35 -0300

    att

--- Exercícios/Exercícios 46 ao 60/Exercícios53.py
+++ Exercícios/Exercícios 46 ao 60/Exercícios53.py
@@ -3,5 +3,10 @@ Crie um programa que leia uma frase qualquer e diga se ela é um palidramo,
 desconsidere os espaços
 '''
 
-texto = str(input('Informe uma frase'))
-    
+texto = str(input('Informe uma frase: ')).upper()
+texto = ''.join(texto.split(' '))
+print('A palavra {} ao contrário é {} e '.format(texto, texto[::-1]))
+if texto == texto [::-1]:
+    print('É um Palíndromo'.format(texto))
+else:
+    print('Não é um Palíndromo'.format(texto))
