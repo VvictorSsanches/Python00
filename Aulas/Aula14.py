376232cb42525dae2c06874ad89993e21be32455
VvictorSsanches <56084555+VvictorSsanches@users.noreply.github.com>
2023-11-08 15:22:54 -0300

    att

---
+++ Aulas/Aula14.py
@@ -0,0 +1,21 @@
+enquanto não maça
+    passo
+pega
+
+maça = 0 
+
+while not maça:
+    print('mais um passo') 
+print('pega')
+pegar = maça
+bloco = +1
+vazio = +2
+moeda = +1
+while not maça:
+    if bloco:
+        print(bloco)
+    if vazio:
+        print(vazio)
+    if moeda:
+        print(moeda)
+print(pegar)  
---
+++ Aulas/Aula14a.py
@@ -0,0 +1,9 @@
+for c in range (10, -1, -1):
+    print(c)
+print('Fim')
+
+c = 1
+while c < 11:
+    print(c)
+    c += 1
+print('Fim')
